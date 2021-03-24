0933e856d5b4479673443d538bf6bfd491658a9a
Sebastian Zapata <you@example.com>
2021-03-24 17:34:29 -0500

    the solution for exercise 2 is deleted

--- src/chapter2/UndefinedParmeterSize.kt
+++ src/chapter2/UndefinedParmeterSize.kt
@@ -1,19 +1,12 @@
 package chapter2
 
 
-fun max(vararg n1: Int): Int{
-    var max= Int.MIN_VALUE
-    val asfda: IntArray =n1
-    for ( n in n1 ){
-        max=if(n>max) n else max
-    }
-
-    return  max
+fun max(n1: Int, n2: Int): Int{
+    return if(n1>n2) n1 else n2
 }
 
 fun main(){
     val res=max(3,5)
     println(res)
     //adapt the function max so it keeps working with 2 parameter but also with n parameteres
-    println(max(1,3,4,5,8,3))
 }
